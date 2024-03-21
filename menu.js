10ec5694a9cebaf27e421aff9b8e47ba0229c2bd
François L.P. <me@francois-lp.dev>
2024-03-21 23:08:33 +0100

    feat: update

--- menu.js
+++ menu.js
@@ -1,13 +1,16 @@
 let menuButton = document.getElementById("menu");
 let menu = document.getElementById("toggle-menu");
+let menuContent = document.getElementById("toggle-menu-content");
 let isMenuToggled = false;
 
 menuButton.addEventListener("click", () => {
     if (isMenuToggled) {
         isMenuToggled = false;
         menu.style.maxHeight = "0";
+        menuContent.style.visibility = "hidden";
     } else {
         isMenuToggled = true;
         menu.style.maxHeight = "200px";
+        menuContent.style.visibility = "visible";
     }
 });
